6ac11dd6a2621589c3a5f65b7b22e0fdbbaa7693
Benjamin Martin <ben.martin@smartpension.co.uk>
2018-10-22 09:55:31 +0100

    tweakage

--- desktop/.screenlayout/work.sh
+++ desktop/.screenlayout/work.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-xrandr --output DP-1-3 --mode 1920x1080 --pos 0x0    --left-of DP-1-1  --rotate normal \
-       --output DP-1-1 --mode 1920x1080 --pos 1920x0 --left-of eDP-1   --rotate normal \
-       --output eDP-1  --mode 3200x1800 --pos 3840x0 --right-of DP-1-1 --rotate normal --primary --scale 0.5x0.5 \
-       --output DP-2 --off \
-       --output DP-1 --off \
-       --output DP-1-2 --off
+xrandr --output eDP-1  --mode 1920x1080 --right-of DP-2-1  --rotate normal --scale 0.7x0.7 \
+       --output DP-2-3 --mode 1920x1080 --left-of DP-2-1  --rotate normal --primary \
+       --output DP-2-1 --mode 1920x1080  --rotate normal
