5f3dd0fd667a5ed058e5bdee743ba8c7658e6dac
Benjamin Martin <ben.martin@smartpension.co.uk>
2018-11-08 17:00:02 +0000

    Added "git arrh" and tweak work screens config

--- desktop/.screenlayout/work.sh
+++ desktop/.screenlayout/work.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1  --mode 1920x1080 --right-of DP-2-1  --rotate normal --scale 0.7x0.7 \
-       --output DP-2-3 --mode 1920x1080 --left-of DP-2-1  --rotate normal --primary \
-       --output DP-2-1 --mode 1920x1080  --rotate normal
+xrandr --output DP-2-3 --mode 1920x1080 --left-of DP-2-1 --primary \
+       --output DP-2-1 --mode 1920x1080 \
+       --output eDP-1  --off
