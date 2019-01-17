087d3dc83f427911b16952137278e962a053f221
Benjamin Martin <ben.martin@smartpension.co.uk>
2019-01-17 14:03:14 +0000

    Tweaks to desktop config

--- desktop/.screenlayout/work.sh
+++ desktop/.screenlayout/work.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-xrandr --output DP-2-3  --left-of DP-2-1 \
-       --output DP-2-1  --primary \
-       --output eDP-1   --right-of DP-2-1
+#xrandr --output DP-2-3  --left-of DP-2-1 \
+#       --output DP-2-1  --primary \
+#       --output eDP-1   --right-of DP-2-1
+
+xrandr --output DP-2 --left-of eDP-1 --primary
